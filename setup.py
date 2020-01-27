210522f3b4f2fa8c75cc4a48d43ba7495820cb8f
Joao Vitor <joaovitor.cn@gmail.com>
2020-01-27 15:20:58 -0300

    update version 27.01.2020 15:00

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pybaf", # Replace with your own username
+    version="0.0.2",
+    author="joaovitocn",
+    author_email="joaovitor.cn@gmail.com",
+    description="Python Distance Calculator Bing Api facilitator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joaovitocn/APIdistances",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
