abbb87d76a1be74c10d3f8e9b9fb17404eb576c5
Joao Vitor <joaovitor.cn@gmail.com>
2020-01-27 15:38:55 -0300

    update version 27.01.2020 15:36

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybaf", # Replace with your own username
-    version="0.0.2",
+    version="0.0.3",
     author="joaovitocn",
     author_email="joaovitor.cn@gmail.com",
     description="Python Distance Calculator Bing Api facilitator",
